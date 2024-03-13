05b786ec897f0b44f1e1aa67678686d8e3e04cac
Mozammal-Hossain-MH <mozammalhossain33@gmail.com>
2024-03-13 22:36:48 +0600

    cart and user collection added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, Db } = require('mongodb');
+const { MongoClient, ServerApiVersion, Db, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,21 +24,53 @@ const client = new MongoClient(uri, {
   }
 });
 
+const usersCollection = client.db('MHSdb').collection('users');
 const menuCollection = client.db('MHSdb').collection('menu');
 const reviewsCollection = client.db('MHSdb').collection('reviews');
+const cartsCollection = client.db('MHSdb').collection('carts');
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // users collection
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    // menu collection
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
 
+    // reviews collection
     app.get('/reviews', async (req, res) => {
       const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    })
+
+    // carts collection
+    app.get('/carts', async (req, res) => {
+      const query = { email: req.query.email };
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartsCollection.insertOne(cartItem);
+      res.send(result);
+    })
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
       res.send(result);
     })
 
