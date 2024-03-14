8e4493654385b7ad4278a11e5061b0fbf9fc5eb5
Mozammal-Hossain-MH <mozammalhossain33@gmail.com>
2024-03-14 15:16:21 +0600

    users api added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, Db, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,7 +9,6 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(cors());
 app.use(express.json());
-
 
 
 
@@ -35,9 +35,43 @@ async function run() {
     await client.connect();
 
     // users collection
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result);
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
+      // insert email if user doesnt exist
+      // you can do this in many ways (unique email, upsert, simple checking)
+      const query = { email: req.body.email }
+      const exist = await usersCollection.findOne(query);
+      if (exist) {
+        return res.send({
+          message: 'user already exist',
+          insertedId: null
+        })
+      }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set : {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
       res.send(result);
     })
 
